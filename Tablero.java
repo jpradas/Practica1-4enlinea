4132264d0d80bf9dd73b0e3ba98801d93388dcea
emanuelr991 <emanuelr@ucm.es>
2014-10-30 01:33:20 +0100

    Update Tablero.java

--- Tablero.java
+++ Tablero.java
@@ -1,75 +1,50 @@
 package logica;
 
 public class Tablero {
-	private Ficha [][] tablero;
-	private int ancho;  // COLUMNAS
-	private int alto;   // FILAS
+	private int filas;
+	private int columnas;
+	private Ficha[][] tablero;
+	private int contCompleto = 0;
 	
-	public Tablero(int alto, int ancho){
-		this.tablero = new Ficha [alto][ancho];
-		this.ancho = ancho;
-		this.alto = alto;
+	public Tablero (int y, int x){
+		this.filas = x; 
+		this.columnas = y;
+		this.tablero = new Ficha[y][x];
+		for (int i = 0; i >= y; i++)
+			for (int j = 0; j >= x; j++)
+				this.tablero[y][x] = Ficha.VACIA;
 	}
 	
-	public void reset(){
-		for (int i = 0; i < this.alto; i++){
-			for (int j = 0; j < this.ancho; j++){
-				this.tablero[i][j] = Ficha.VACIA;
-			}
+	public void setCont(int x ){
+		this.contCompleto = x;
+	}
+	public int getCont (){
+		return this.contCompleto;
+	}
+	
+	public void setFicha(int y, int x,Ficha ficha){
+		this.tablero[y][x] = ficha;
+		this.contCompleto++; //hacer algo parecido en la pila y el undo
+	}
+	
+	public Ficha getFicha(int y, int x){
+		return this.tablero[y][x];
+	}
+	
+	public boolean completo(){
+		boolean completo=false;
+		int numFichas = this.filas*this.columnas;
+		if (this.contCompleto >= numFichas){
+			completo = true;
 		}
-	}
-	
-	public int devolverAlturaDesocupada(int col){
-		int i = this.alto - 1;
-		boolean encontrado = false;
-		while(i < this.alto && i >= 0 && !encontrado){
-			if (this.tablero[i][col] == Ficha.VACIA){
-				encontrado = true;
-			}
-			else{
-				i--;
-			}
-		}
-		return i;
-	}
-	
-	public void setFicha(Ficha ficha, int col, int altura){
-		this.tablero[altura][col] = ficha;
-	}
-	
-	public boolean hayGrupo(){
-		boolean hayGrupo = false;
-		int consecutivos = 0;
-		for(int i = 0; i < this.alto; i++){
-			for(int j = 0; j < this.ancho; j++){
-				if((j+1 < this.ancho) && this.tablero[i][j] == this.tablero[i][j+1] && this.tablero[i][j] != Ficha.VACIA){ 
-	                consecutivos = 0;
-					while((j+consecutivos < this.ancho-1) && this.tablero[i][j] == this.tablero[i][j+consecutivos]){ 
-	                    consecutivos++; 
-	                } 
-	                if(consecutivos >= 4 && this.tablero[i][j] == this.tablero[i][j+1]){ 
-	                    hayGrupo = true; 
-	                } 
-	            } 
-	            else if((i+1 < this.alto) && this.tablero[i][j] == this.tablero[i+1][j] && this.tablero[i][j] != Ficha.VACIA){ 
-	                consecutivos = 0; 
-	                while((i+consecutivos < this.alto) && this.tablero[i][j] == this.tablero[i+consecutivos][j]){ 
-	                    consecutivos++; 
-	                }  
-	                if(consecutivos >= 4 && this.tablero[i][j] == this.tablero[i+1][j]){ 
-	                    hayGrupo = true; 
-	                } 
-	            } 
-			}
-		}
-		return hayGrupo;
+		return completo;
 	}
 	
 	public String toString(){
 		String mensaje = "";
 		String nuevalinea = System.getProperty("line.separator");
-		for(int i = 0; i < this.alto; i++){
-			for(int j = 0; j < this.ancho; j++){
+		for(int i = 0; i < this.filas; i++){
+			for(int j = 0; j < this.columnas; j++){
 				if(this.tablero[i][j] == Ficha.VACIA){
 					mensaje = mensaje + "O ";
 				}
@@ -83,5 +58,12 @@ public class Tablero {
 			mensaje = mensaje + nuevalinea;
 		}
 		return mensaje;
+	}
+	
+	public int getAlturaVacia(int x){
+		int alt = this.filas - 1;
+		while (this.tablero[alt][x]!= Ficha.VACIA)
+			alt--;
+		return alt;	  
 	}
 }
