a42e9c368369ed9369bb5d30cc592161b10f1415
jpradas <jpradas@ucm.es>
2014-11-01 00:17:56 +0100

    Update Ficha.java

--- Ficha.java
+++ Ficha.java
@@ -1,5 +1,8 @@
-package practica1;
+package logica;
 
 public enum Ficha {
+	/**
+	 * La clase enumerada Ficha crea el objeto llamado Ficha con 3 posibles valores: VACIA, BLANCA O NEGRA.
+	 */
 	VACIA, BLANCA, NEGRA
 }
