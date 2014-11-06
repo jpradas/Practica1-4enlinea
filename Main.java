ce3fb290a0dfde9ec7fc3c951ed7f557dcb780fd
emanuelr991 <emanuelr@ucm.es>
2014-11-06 01:46:23 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,12 +3,14 @@ import java.util.Scanner;
 import logica.Partida;
 import control.Controlador;
 
-public class Main {
-	private static Scanner in = new Scanner(System.in);
 
+public class Main {
+	
+	private static Scanner in = new Scanner(System.in);
+	
 	public static void main(String[] args) {
 		Partida partida = new Partida();
-		Controlador c = new Controlador(partida,in);
+		Controlador c = new Controlador (partida,in);
 		c.run();
 	}
 
