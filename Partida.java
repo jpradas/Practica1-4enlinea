f2e0346cdef6c1f7117c471bab3d33905488c50e
jpradas <jpradas@ucm.es>
2014-11-13 17:58:53 +0100

    Update Partida.java

--- Partida.java
+++ Partida.java
@@ -2,41 +2,64 @@ package logica;
 
 
 public class Partida {
+	
 	private Tablero tablero;
 	private Ficha turno = Ficha.BLANCA;
 	private boolean terminada = false;
 	private Ficha ganador = Ficha.VACIA;
-	private int[] undoStack = new int [42];		
+	public static final int MAX_UNDO = 10;
+	private int[] undoStack = new int [FILAS*COLUMNAS];		
 	private int numUndo = 0;
+	private int minUndo = 0;
 	private final static int FILAS = 6;
 	private final static int COLUMNAS = 7;
+	
+
+	/**
+	 * La constructora Partida crea un tablero con un numero determinado de filas y columnas. 
+	 */
 	
 	public Partida(){
 		this.tablero = new Tablero(FILAS, COLUMNAS);
 	}
 	
+	/**
+	 * El metodo getFichaGanador devuelve la ficha ganadora.
+	 * @return Retorna la ficha ganadora.
+	 */
 	public Ficha getFichaGanador(){
 		return this.ganador;
 	}
 	
+	/**
+	 * El metodo terminada retorna el booleano "terminada".
+	 * @return Retorna el booleano terminada indicando si la partida ha terminado.
+	 */
 	public boolean terminada(){
 		return this.terminada;
 	}
 	
+	/**
+	 * El metodo reset reinicia el tablero volviendo a su condicion original con todas las fichas vacias.
+	 */
 	public void reset(){
 		this.tablero.reset();
 		this.turno = Ficha.BLANCA;
 	}
 	
+	/**
+	 * El metodo desHacer deshace un movimiento del usuario, tantos como se hayan hecho desde el inicio de la partida.
+	 * @return Retorna un booleano indicando si el metodo "deshacer" ha podido realizarse.
+	 */
 	public boolean desHacer(){
 		boolean valido = false;
-		if(this.numUndo <= 0){
+		if(this.numUndo <= 0 || this.minUndo == this.numUndo){
 			valido = false;
 		}
 		else{
 			int altura = this.tablero.devolverAlturaDesocupada(this.undoStack[this.numUndo - 1]) + 1;
 			this.tablero.setFicha(Ficha.VACIA, this.undoStack[this.numUndo - 1] , altura);
-			this.numUndo = this.numUndo - 1;
+			this.numUndo--;
 			valido = true;
 		}
 		if(this.turno == Ficha.BLANCA){
@@ -48,8 +71,13 @@ public class Partida {
 		return valido;
 	}
 	
+	/**
+	 * El metodo ejecutaMovimiento comprueba si el movimiento que queremos efectuar entra dentro de los parametros y en caso afirmativo lo ejecuta actualizando el tablero.
+	 * @param col Es la columna en la cual queremos añadir una ficha.
+	 * @return Retorna un booleano indicando si el movimiento es valido
+	 */
 	public boolean ejecutaMovimiento(int col){
-		boolean moValido = false;
+		boolean moValido = true;
 		if(col < 0 || col >= COLUMNAS){
 			moValido = false;
 		}
@@ -57,10 +85,10 @@ public class Partida {
 			int altura = this.tablero.devolverAlturaDesocupada(col);
 			if(altura <= FILAS && altura >= 0){
 				this.tablero.setFicha(this.turno, col, altura);
-				moValido = true;
 				this.undoStack[numUndo] = col;
-				numUndo++;
-				if(numUndo == 42){
+				this.numUndo++;
+				this.minUndo = this.numUndo - 10;
+				if(numUndo == FILAS*COLUMNAS){
 					this.terminada = true;
 					this.ganador = Ficha.VACIA;
 				}
@@ -84,6 +112,10 @@ public class Partida {
 		return moValido;
 	}
 	
+	/**
+	 * El metodo hayGrupo comprueba en el tablero si hay un grupo de 4 fichas en el tablero.
+	 * @return Retorna un booleano que indica si hay un grupo de 4 fichas en el tablero.
+	 */
 	private boolean hayGrupo(){
 		boolean hayGrupo = false;
 		int consecutivos = 0;
