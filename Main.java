dc2efeba566b0d7c0df4a4f106a897268fcd7796
emanuelr991 <emanuelr@ucm.es>
2014-11-13 18:03:10 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,6 +4,11 @@ import logica.Partida;
 import control.Controlador;
 
 
+/** Primera practica de la asignatura Tecnologia de la Programacion del Grupo 2B
+ * @author Emanuel Ramirez, Josue Pradas
+ * @version Practica 1 TP , V1.0
+ * Clase main que contiene la clase principal del programa, asi como la llamada inicial.
+ */
 public class Main {
 	
 	private static Scanner in = new Scanner(System.in);
