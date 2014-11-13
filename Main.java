c26f071fe7fa78425fc7a265a2e553cc4fa00616
jpradas <jpradas@ucm.es>
2014-11-13 17:59:41 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,6 +6,10 @@ import control.Controlador;
 public class Main {
 	private static Scanner in = new Scanner(System.in);
 
+	/**
+	 * Inicializa la aplicacion y crea la partida
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		Partida partida = new Partida();
 		Controlador c = new Controlador(partida,in);
