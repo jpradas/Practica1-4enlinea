f3330218c25f6020e18e3cf21a092286ced7f2f2
emanuelr991 <emanuelr@ucm.es>
2014-11-14 09:45:39 +0100

    Update Tablero.java

--- Tablero.java
+++ Tablero.java
@@ -5,8 +5,14 @@ public class Tablero {
 	private int filas;
 	private int columnas;
 	private Ficha[][] tablero;
-	private int contCompleto = 0;
+	private int contCompleto = 0; //Contador para saber cuantas fichas han sido puestas
 	
+	/**
+	 * La constructora Tablero implementa el tablero del juego con enumerados del tipo Ficha e inicializa los atributos privados alto y ancho al valor de la matriz creada.
+	 * E inicializa la matriz a todas posiciones VACIA
+	 * @param y	El parametro alto indica la altura que tendra el tablero.
+	 * @param x	El parametro ancho indica la anchura que tendra el tablero.
+	 */
 	public Tablero (int y, int x){
 		this.filas = y; 
 		this.columnas = x;
@@ -16,22 +22,41 @@ public class Tablero {
 				this.tablero[i][j] = Ficha.VACIA;
 	}
 	
+	/** Colocar contador, cambia el valor de contador completo
+	 * @param cont numero de fichas puestas
+	 */
 	public void setCont(int cont){
 		this.contCompleto = cont;
 	}
+	/** Obtiene el valor de contCompleto
+	 * @return regresa el contCompleto que es el numero de fichas jugadas
+	 */
 	public int getCont (){
 		return this.contCompleto;
 	}
 	
+	/** Coloca la ficha en la posicion (y,x)
+	 * @param y posicion de la ficha en el eje y 
+	 * @param x posicion de la ficha en el eje x 
+	 * @param ficha ficha correspondiente al turno 
+	 */
 	public void setFicha(int y, int x,Ficha ficha){
 		this.tablero[y][x] = ficha;
 		this.contCompleto++;
 	}
 	
+	/** Devuelve la ficha en la posicion (y,x)
+	 * @param y posicion de la ficha en el eje y 
+	 * @param x posicion de la ficha en el eje x 
+	 * @return Ficha en esa posicion 
+	 */
 	public Ficha getFicha(int y, int x){
 		return this.tablero[y][x];
 	}
 	
+	/** Devuelve verdadero si el tablero esta completo usando el contCompleto y el numero de fichas totales
+	 * @return Verdadero o Falso si el tablero esta lleno
+	 */
 	public boolean completo(){
 		boolean completo=false;
 		int numFichas = this.filas*this.columnas;
@@ -41,6 +66,9 @@ public class Tablero {
 		return completo;
 	}
 	
+	/**
+	 * El metodo toString devuelve la informacion de la clase tablero en forma de String.
+	 */
 	public String toString(){
 		String mensaje = "";
 		String nuevalinea = System.getProperty("line.separator");
@@ -61,6 +89,10 @@ public class Tablero {
 		return mensaje;
 	}
 	
+	/** Devuelve la altura a la cual se encuentra la primera ficha vacia de determinada columna
+	 * @param col colunma en la cual se quiere encontrar la altura vacia
+	 * @return altura vacia en dicha columna 
+	 */
 	public int getAlturaVacia(int col){
 		int alt = this.filas - 1;
 		while (alt >= 0 && this.tablero[alt][col]!= Ficha.VACIA)
