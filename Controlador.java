7430aa635a8ec7802ee01d99b4ccc1da497f5afd
jpradas <jpradas@ucm.es>
2014-12-10 13:42:26 +0100

    Update Controlador.java

--- Controlador.java
+++ Controlador.java
@@ -1,27 +1,62 @@
-package practica1;
+package control;
 
 import java.util.Scanner;
+
+import logica.Ficha;
+import logica.MovimientoComplica;
+import logica.MovimientoConecta4;
+import logica.Partida;
+import logica.ReglasJuegoComplica;
+import logica.ReglasJuegoConecta4;
+import logica.Movimiento;
 
 public class Controlador {
 	private Partida partida;
 	private Scanner in;
+	private TipoJuego jugandoA;
 	
-	public Controlador(Partida partida, Scanner in){
+	public Controlador(Partida partida, Scanner in, TipoJuego juego){
 		this.partida = partida;
 		this.in = in;
+		this.jugandoA = juego;
 	}
 	
 	public void run(){
-		this.partida.reset();
+		//this.partida.reset(new ReglasJuegoConecta4());
 		String opcion = "";
 		while(!opcion.equalsIgnoreCase("SALIR") && !this.partida.terminada()){
 			System.out.println(this.partida.toString());
 			System.out.println("Que quieres hacer? ");
-			opcion = this.in.next();
+			opcion = null;
+			opcion = this.in.nextLine();
 			if(opcion.equalsIgnoreCase("PONER")){
 				System.out.println("-- Introduce la columna: ");
 				int col = this.in.nextInt() - 1;
-				if(!this.partida.ejecutaMovimiento(col)){
+				if(this.jugandoA == TipoJuego.CONECTA4){
+					Movimiento mov = new MovimientoConecta4(col, this.partida.getJugador());
+					if(!this.partida.ejecutaMovimiento(mov)){
+						System.err.println("Movimiento incorrecto");
+					}
+					else{
+						if(this.partida.terminada()){
+							if(this.partida.getFichaGanador() == Ficha.VACIA){
+								System.out.println("Partida en tablas");
+								opcion = "salir";
+							}
+							else{
+								System.out.println("Gana la " + this.partida.getFichaGanador());
+								opcion = "salir";
+							}
+						}
+					}
+				}
+				else if(this.jugandoA == TipoJuego.COMPLICA){
+					Movimiento mov = new MovimientoComplica(col, this.partida.getJugador());
+					if(!this.partida.ejecutaMovimiento(mov)){
+						System.err.println("Movimiento incorrecto");
+					}
+				}
+				/*if(!this.partida.ejecutaMovimiento(col)){
 					System.out.println("Movimiento incorrecto");
 				}
 				else{
@@ -32,11 +67,11 @@ public class Controlador {
 							opcion = "salir";
 						}
 						else{
-							System.out.println("Ganan las " + ganador);
+							System.out.println("Gana la " + ganador);
 							opcion = "salir";
 						}
 					}
-				}
+				}*/
 			}
 			else if(opcion.equalsIgnoreCase("DESHACER")){
 				if(!this.partida.desHacer()){
@@ -44,9 +79,21 @@ public class Controlador {
 				}
 			}
 			else if(opcion.equalsIgnoreCase("REINICIAR")){
-				this.partida.reset();
+				if(this.jugandoA == TipoJuego.CONECTA4){
+					this.partida.reset(new ReglasJuegoConecta4());
+				}
+				else{
+					this.partida.reset(new ReglasJuegoComplica());
+				}
+			}
+			else if(opcion.equalsIgnoreCase("JUGAR C4")){
+				this.jugandoA = TipoJuego.CONECTA4;
+				this.partida.reset(new ReglasJuegoConecta4());
+			}
+			else if(opcion.equalsIgnoreCase("JUGAR CO")){
+				this.jugandoA = TipoJuego.COMPLICA;
+				this.partida.reset(new ReglasJuegoComplica());
 			}
 		}
 	}
-	
 }
